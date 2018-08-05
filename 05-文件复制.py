f8f39d83c97c5757862de7a01c48c448c84eb381
Zhijie <jimmy-lee_93.06@hotmail.com>
2018-08-05 12:05:53 +0200

    文件复制三种方式

--- 05-文件复制.py
+++ 05-文件复制.py
@@ -1,17 +1,31 @@
 #0.提示并获取要复制的文件名
-name = input("请输入要复制的文件名")
+name = input("请输入要复制的文件名:")
 
 
 #1.打开要复制的文件
 f_read = open(name,"r")
 
 #2.创建一个新的文件，用来存储源文件的数据内容
-new_name = 
-f_write = open("test[复件].txt","w")
+find_position = name.rfind(".")
+new_name = name[:find_position]+"[复件]"+name[find_position:]
+f_write = open(new_name,"w")
 
 #3.复制
-content = f_read.read()
-f_write.write(content)
+#第一种方式
+#content = f_read.read()
+#f_write.write(content)
+
+#第二种方式
+#for line_content in f_read.readlines(): 
+#	f_write.write(line_content)
+
+#第三种方式
+while True:
+	line_content = f_read.readline()
+	if len(line_content)>0:
+		f_write.write(line_content)
+	else:
+		break
 
 #4.关闭文件
 
