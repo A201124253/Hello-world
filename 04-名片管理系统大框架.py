9d1ba69424b0dfe153f6ba281477cf853d3bb33f
A201124253 <jimmy-lee_93.06@hotmail.com>
2018-08-06 17:03:44 +0200

    递归调用

---
+++ 04-名片管理系统大框架.py
@@ -0,0 +1,22 @@
+#1.打印功能提示
+print("="*50)
+print("名片管理系统 V0.01")
+print("1.添加一个新的名片")
+print("2.删除一个名片")
+print("3.修改一个名片")
+print("4.查询一个名片")
+print("5.推出系统")
+print("="*50)
+#2.获取用户的输入
+num = int(input("请输入操作序号："))
+#3.根据用户的数据执行相应的功能
+if num==1:
+	pass
+if num==2:
+	pass
+if num==3:
+	pass
+if num==4:
+	pass
+if num==5:
+	pass
