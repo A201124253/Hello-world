a95c4aca9005a6b3d300b041d4e317374e022d35
A201124253 <jimmy-lee_93.06@hotmail.com>
2018-08-09 14:57:14 +0200

    存放家具

--- 06-面向对象/05-存放家具.py
+++ 06-面向对象/05-存放家具.py
@@ -1,10 +1,18 @@
 class Home:
 	def __init__(self, area):
 		self.area = area
-
+		self.contains_item = []
 	def __str__(self):
 		msg = "家当前可用面积为："+str(self.area)
+		msg += '\n'
+		msg += "家里的物品有："
+		
 		return msg
+	
+	def add_item(self,item):
+		if self.area>item.area:
+			self.area-=item.area
+			self.contains_item.append(item)
 
 class Bed:
 	def __init__(self, name, area):
@@ -21,3 +29,4 @@ print(home)
 bed = Bed("席梦思", 4)
 print(bed)
 
+home.add_item(bed)
