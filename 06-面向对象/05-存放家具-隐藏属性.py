d8fe03a3d1765c67f79c3682311a5f4c4ac49528
A201124253 <jimmy-lee_93.06@hotmail.com>
2018-08-11 10:00:12 +0200

    隐藏属性

---
+++ 06-面向对象/05-存放家具-隐藏属性.py
@@ -0,0 +1,54 @@
+class Home:
+	def __init__(self, area):
+		self.area = area
+		self.contains_item = []
+						
+	def __str__(self):
+		msg = "家当前可用面积为："+str(self.area)
+		msg += '\n'
+		if len(self.contains_item)>0:
+			msg += "家里的物品有："
+		
+		for temp in self.contains_item:
+			msg +=temp.get_name()+ ","
+		
+		msg = msg[:-1]
+
+		return msg
+	
+	def add_item(self,item):
+		area = item.get_area()
+		if self.area>area:
+			self.area-=area
+			self.contains_item.append(item)
+
+class Bed:
+	def __init__(self, name, area):
+		self.name = name
+		self.area =area
+	
+	def __str__(self):
+		msg = self.name + "床的面积为：" + str(self.area)
+		return msg
+
+	def get_name(self):
+		return self.name
+	
+	def get_area(self):
+		return self.area
+
+#创建一个家对象
+home = Home(128)
+print(home)
+
+bed = Bed("席梦思", 4)
+print(bed)
+
+home.add_item(bed)
+print(home)
+
+bed2 = Bed("硬板床", 3)
+print(bed2)
+
+home.add_item(bed2)
+print(home)
